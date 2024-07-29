7128a93ffb550207c58ecc0948222244440e1722
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2024-07-29 22:25:44 +0500

    resposiveness

--- src/components/Section/BannerSection/BannerSectionStyle4.jsx
+++ src/components/Section/BannerSection/BannerSectionStyle4.jsx
@@ -9,17 +9,35 @@ export default function BannerSectionStyle4({
 }) {
   return (
     <div className="container">
-      <div
-        className={`cs_banner cs_style_4 cs_bg_filed overflow-hidden ${center ? 'text-center' : ''}`}
-        style={{ backgroundImage: `url(${why_us})` }}
-      >
-        <h2 className="cs_banner_title cs_main_heading_color cs_fs_72">
-          {parser(title)}
-        </h2>
-        <p className="cs_banner_subtitle para_color cs_fs_20 m-0">
-          {parser(subTitle)}
-        </p>
-      </div>
+    <div
+  className={`cs_banner cs_style_4 cs_bg_filed overflow-hidden ${center ? 'text-center' : ''}`}
+  style={{ position: 'relative' }}
+>
+  <div
+    style={{
+      backgroundImage: `url(${why_us})`,
+      filter: 'blur(8px)',
+      WebkitFilter: 'blur(3px)',
+      height: '100%',
+      width: '100%',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex: -1,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}
+  ></div>
+  <div style={{ position: "relative", top: "10rem" }}>
+    <h2 className="cs_banner_title cs_main_heading_color cs_fs_72">
+      {parser(title)}
+    </h2>
+    <p className="cs_banner_subtitle para_color cs_fs_20 m-0">
+      {parser(subTitle)}
+    </p>
+  </div>
+</div>
+
     </div>
   );
 }
--- src/components/Testimonial/TestimonialStyle2.jsx
+++ src/components/Testimonial/TestimonialStyle2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Rating from '../Rating';
+// import Rating from '../Rating';
 
 export default function TestimonialStyle2({
   text,
@@ -10,7 +10,7 @@ export default function TestimonialStyle2({
 }) {
   return (
     <div className="cs_testimonial cs_style_2">
-      <div className="cs_testimonial_text cs_radius_25 cs_white_bg cs_fs_24">
+      <div className="cs_testimonial_text cs_radius_25 cs_white_bg cs_fs_24" style={{height:"28rem"}}>
         <div className="cs_quote_icon">
           <svg
             width={50}
@@ -29,8 +29,8 @@ export default function TestimonialStyle2({
             />
           </svg>
         </div>
-        <p>{text}</p>
-        <Rating ratingNumber={ratingNumber} />
+        <p style={{textAlign:'justify'}}>{text}</p>
+        {/* <Rating ratingNumber={ratingNumber} /> */}
       </div>
       <div className="cs_testimonial_meta">
         <div className="cs_testimonial_avatar">
