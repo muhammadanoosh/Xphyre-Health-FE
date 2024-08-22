a32ccb4ffb42e763627e44ab9d216836f130bbc5
muhammadanoosh <muhammadanoosh.ma@gmail.com>
2024-08-23 00:21:18 +0500

    responsiveness

--- src/components/IconBox/IconBoxStyle4.jsx
+++ src/components/IconBox/IconBoxStyle4.jsx
@@ -19,13 +19,30 @@ export default function IconBoxStyle4({ title, description }) {
   const shortDescription = getShortDescription(description, wordLimit);
 
   return (
-    <div className="cs_iconbox cs_style_4">
+    <div
+      className="cs_iconbox cs_style_4"
+      style={{
+        height: isExpanded ? '100%' : '13rem',
+        transition: 'height 0.3s ease',
+      }}
+    >
       <h2 className="cs_iconbox_title cs_fs_32 cs_main_heading_color">{title}</h2>
       <div>
-        <p className="cs_iconbox_subtitle m-0 para_color" >
+        <p
+          className="cs_iconbox_subtitle m-0 para_color"
+          style={{
+            maxHeight: isExpanded ? '100rem' : '3rem', // Adjust the maxHeight for text expansion
+            overflow: 'hidden',
+            transition: 'max-height 0.3s ease, opacity 0.3s ease',
+            opacity: isExpanded ? 1 : 0.8, // Adjust the opacity for a smooth effect
+          }}
+        >
           {isExpanded ? description : shortDescription}
         </p>
-        <button style={{ border: "none", borderRadius: "0.5rem" }} onClick={toggleExpand}>
+        <button
+          style={{ border: 'none', borderRadius: '0.5rem' }}
+          onClick={toggleExpand}
+        >
           {isExpanded ? 'Read Less' : 'Read More'}
         </button>
       </div>
